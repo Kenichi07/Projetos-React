8613ecf2fe6c5115132f4acbee21da33bc61b513
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-29 23:29:20 -0300

    feat: add consuming api

--- Codeconnect/src/App.jsx
+++ Codeconnect/src/App.jsx
@@ -4,8 +4,18 @@ import BarraDePesquisa from "./componentes/BarraDePesquisa";
 import Filtro from "./componentes/Filtro";
 import Ordenacao from "./componentes/Ordenacao";
 import Card from "./componentes/Card";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes"
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => setDados(dados));
+  }, []);
+
   return (
     <div className="container">
       <Sidebar />
@@ -13,7 +23,24 @@ function App() {
         <BarraDePesquisa />
         <Filtro />
         <Ordenacao />
-        <Card />
+        <ul className="lista-cards">
+          {dados
+            ? dados.map((item, index) => (
+                <li key={index}>
+                  <Card
+                    id={item.id}
+                    imagemUrl={item.imagem_capa}
+                    titulo={item.titulo}
+                    resumo={item.resumo}
+                    linhasDeCodigo={item.linhas_de_codigo}
+                    compartilhamentos={item.compartilhamentos}
+                    comentarios={item.comentarios}
+                    usuario={item.usuario}
+                  />
+                </li>
+              ))
+            : null}
+        </ul>
       </div>
     </div>
   );
