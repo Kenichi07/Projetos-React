5f4ddb3c4085b64775dd7311b788d20a560fe300
Vinicius Huang <viniciushuangbr@gmail.com>
2025-11-03 12:33:16 -0300

    feat: created and styled Colaborador component

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = (props) => {
@@ -7,6 +8,8 @@ const Time = (props) => {
   return (
     <section className="time" style={corSecundaria}>
       <h3 style={corPrimaria}>{props.nome}</h3>
+      <Colaborador />
+      <Colaborador />
     </section>
   );
 };
