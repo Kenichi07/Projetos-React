bb94329ce1b3908d84d072606503db75b6069ac2
Vinicius Huang <viniciushuangbr@gmail.com>
2025-11-03 23:27:52 -0300

    style: styled Colaborador component about cabecalho

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -3,14 +3,15 @@ import "./Time.css";
 
 const Time = (props) => {
   const corSecundaria = { backgroundColor: props.corSecundaria };
-  const corPrimaria = { borderColor: props.corPrimaria };
 
-  return (
-    props.colaboradores.length > 0 ? <section className="time" style={corSecundaria}>
-      <h3 style={corPrimaria}>{props.nome}</h3>
+  return props.colaboradores.length > 0 ? (
+    <section className="time" style={corSecundaria}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className="colaboradores">
         {props.colaboradores.map((colaborador) => (
           <Colaborador
+            corDeFundo={props.corPrimaria}
+            key={colaborador.nome}
             nome={colaborador.nome}
             cargo={colaborador.cargo}
             imagem={colaborador.imagem}
@@ -18,7 +19,8 @@ const Time = (props) => {
         ))}
       </div>
     </section>
-    : "" 
+  ) : (
+    ""
   );
 };
 
