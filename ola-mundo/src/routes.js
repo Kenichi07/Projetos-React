bc80ce65bb71d963bf5afe7cf9ab8a279e898b74
Vinicius Huang <viniciushuangbr@gmail.com>
2025-11-14 20:11:04 -0300

    feat: created and styled NaoEncontrada page

--- ola-mundo/src/routes.js
+++ ola-mundo/src/routes.js
@@ -5,6 +5,7 @@ import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Post from "paginas/Post";
+import NaoEncontrada from "paginas/NaoEncontrada";
 
 function AppRoutes() {
   return (
@@ -29,7 +30,7 @@ function AppRoutes() {
             <SobreMim />
           </PaginaPadrao>
         */}
-        <Route path="*" element={<div>404 - Página Não Encontrada</div>} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
       <Rodape />
     </BrowserRouter>
