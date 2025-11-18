881221af7d507f7d06edac8f7e24a4d3b50a0310
Vinicius Huang <viniciushuangbr@gmail.com>
2025-11-17 22:10:07 -0300

    styled: styled tags components

--- space-app/src/componentes/Galeria/Tags/index.jsx
+++ space-app/src/componentes/Galeria/Tags/index.jsx
@@ -1,13 +1,33 @@
+import styled from "styled-components";
 import tags from "./tags.json";
+
+const TagTitulo = styled.h3`
+  color: #d9d9d9;
+  font-size: 24px;
+  margin: 0;
+`;
+
+const Tag = styled.button`
+  font-size: 24px;
+  color: #ffffff;
+  background: rgba(217, 217, 217, 0.3);
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  padding: 12px;
+  box-sizing: border-box;
+  border: 2px solid transparent;
+  &:hover {
+    border-color: #c98cf1;
+  }
+`;
 
 const Tags = () => {
   return (
     <>
-      <p>Busque por tags:</p>
+      <TagTitulo>Busque por tags:</TagTitulo>
       {tags.map((tag) => (
-        <button key={tag.id} className="tag-button">
-          {tag.titulo}
-        </button>
+        <Tag key={tag.id}>{tag.titulo}</Tag>
       ))}
     </>
   );
