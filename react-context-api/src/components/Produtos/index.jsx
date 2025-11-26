39b667fb548872b9b4792ef160c42d4837988f36
Vinicius Huang <viniciushuangbr@gmail.com>
2025-11-25 22:32:49 -0300

    refactor: refactor adicionarProduto and removerProduto in hook

--- react-context-api/src/components/Produtos/index.jsx
+++ react-context-api/src/components/Produtos/index.jsx
@@ -1,8 +1,10 @@
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import { useCarrinhoContext } from "../../hooks/useCarrinhoContext";
 
 const Produtos = () => {
+  const { adicionarProduto } = useCarrinhoContext();
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
